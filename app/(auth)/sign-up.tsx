e577005e3136afbfe4f09a61c8d0c86e4e4922f2
BONAM HARISH NAIDU <Harish1611@users.noreply.github.com>
2026-04-05 16:23:31 +0530

    New: Implemented Tab Layout & SafeAreaView Functionality

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -2,12 +2,12 @@ import {View, Text} from 'react-native'
 import React from 'react'
 import {Link} from "expo-router";
 
-const SignIn = () => {
+const SignUp = () => {
     return (
         <View>
             <Text>SignIn</Text>
-            <Link href="/(auth)/sign-in"> Create Account</Link>
+            <Link href="/(auth)/sign-up"> Sign Up</Link>
         </View>
     )
 }
-export default SignIn
+export default SignUp
