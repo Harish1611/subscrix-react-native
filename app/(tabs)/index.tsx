5598435480ace4846beb2ff47b16140b080486bd
BONAM HARISH NAIDU <Harish1611@users.noreply.github.com>
2026-04-09 22:47:30 +0530

    New: Implement custom fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import "@/global.css"
-import { Text, View } from "react-native";
+import { Text } from "react-native";
 
 import {Link} from "expo-router";
 import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
@@ -8,9 +8,11 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
     return (
         <SafeAreaView className='flex-1 bg-background p-5'>
-            <Text className="text-xl font-bold text-success">
-                Welcome to Nativewind!
+            <Text className="text-5xl font-sans-extrabold">
+                Home
             </Text>
+
+
 
             <Link href='/onboarding' className='mt-4 rounded bg-primary text-white p-4'>Go to Onboarding</Link>
             <Link href='/(auth)/sign-in' className='mt-4 rounded bg-primary text-white p-4'>Go to Sign In</Link>
